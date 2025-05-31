5409d03169038093c5b8a0e6a16177c52fb4ef11
Fadidab <fadidab1998@gmail.com>
2025-05-31 22:49:37 +0200

    wbmailer testing

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -26,14 +26,13 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-EMAIL_HOST_USER=consultation.alhakim@gmail.com
-EMAIL_HOST_PASSWORD=bmmviyhxntrcnwnd
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")bmmviyhxntrcnwnd
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 
