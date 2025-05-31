3bd05e9fda0eedddc0bea7e85873b9251ad03818
Fadidab <fadidab1998@gmail.com>
2025-05-31 21:31:38 +0200

    testin webmailer

--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+import os
+
+from dotenv import load_dotenv
 import cloudinary.uploader
 from django.conf import settings
 from django.contrib import messages
@@ -20,7 +23,7 @@ from specialties.models import Specialty
 
 from .forms import ConsultationForm, ContactForm, DocumentsForm
 from .models import Consultation, ConsultationHistory, Document
-
+load_dotenv()
 
 class Index(generic.ListView):
     model = Doctor
@@ -55,7 +58,7 @@ class Index(generic.ListView):
             send_mail(
                 subject,
                 body,
-                settings.EMAIL_HOST_USER,
+                os.environ.get("EMAIL_HOST_USER"),
                 [
                     "berrofouad66@gmail.com",
                 ],
